5ab2f24a5d7826ae0e13cb813d066b693a1fb422
SHHS-Calkins <12359618@c-vusd.org>
2026-01-12 18:07:18 +0000

    World's best comments

--- Position.java
+++ Position.java
@@ -1,34 +1,35 @@
-public class Position {
-  
-  private double x;
-  private double y;
-  private double distance;
-  
-  public Position(double xVal, double yVal) {
-    x = xVal;
-    y = yVal;
+public class Position { //Class Name
+  //Instance Variables
+  private double x; //Left/Right position
+  private double y; //Up/Down position
+  private double distance; //Total distance traveled. 
+  //Constructor
+  public Position(double xVal, double yVal) { //Creates an object with two values.
+    x = xVal; //This object's x value.
+    y = yVal; //This object's y value.
   }
-  public String toString() {
-    return "(" + x + ", " + y + ")";
+  //Method Stuffs
+  public String toString() { //Prints Coordinates.
+    return "(" + x + ", " + y + ")"; //The making of the coordinate string.
   }
-  public double totalDistance() {
-    return distance;
+  public double totalDistance() { //Returns distance traveled.
+    return distance; //The thing that was just said.
   }
-  public void move(double mDistance, String direction) {
-    distance += mDistance;
-    switch(direction) {
-      case "N": 
-        y += mDistance;
-        break;
-      case "W": 
-        x -= mDistance;
-        break;
-      case "S": 
-        y -= mDistance;
-        break;
-      case "E": 
-        x += mDistance;
-        break;
+  public void move(double mDistance, String direction) { //Move by mDistance(Moving Distance) in direction.
+    distance += mDistance; //Add whatever distance was entered into the method to the Total.
+    switch(direction) { //Switch-case is an alternative for multiple If-Elses.
+      case "N": //If the value's "N",
+        y += mDistance; // Add to y (Go Up)
+        break; //Get out of the switch.
+      case "W": //If the value's "W",
+        x -= mDistance; // Subtract From x (Go Left)
+        break; //Get out of the switch.
+      case "S": //If the value's "S",
+        y -= mDistance; // Subtract From y (Go Down)
+        break; //Get out of the switch.
+      case "E": //If the value's "E",
+        x += mDistance; // Add to x (Go Right)
+        break; //Get out of the switch.
     }
   }
 }
--- Test.java
+++ Test.java
@@ -1,36 +1,36 @@
-import java.util.Scanner;
-
-public class Test {
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        System.out.println("Creation Of Position Object X: ");
-        double choice1 = scan.nextDouble();
-        System.out.println("Creation Of Position Object Y: ");
-        double choice2 = scan.nextDouble();
-        Position p = new Position(choice1, choice2);
-        System.out.println(p);
-        System.out.println("Method (q to quit)");
+import java.util.Scanner; //Scanner implementation
+public class Test { //A public class, it's called a test.
+    public static void main(String[] args) { //Main class Nonsense
+        Scanner scan = new Scanner(System.in); //Scanner Thingie
+        System.out.println("Creation Of Position Object X: "); //Something to be user-friendly
+        double choice1 = scan.nextDouble(); //Takes x value
+        System.out.println("Creation Of Position Object Y: "); //More user-friendly stuff
+        double choice2 = scan.nextDouble(); //Taking the y value.
+        Position p = new Position(choice1, choice2); //Makes a Position object using X and Y values
+        System.out.println(p); //Prints out what that object is just to check.
+        System.out.println("Method (q to quit)"); //User-friendly for Friendly-User
         while (true) { //Forever Loop (Until "q" is chosen).
-            String method = scan.nextLine();
-            switch (method) {
-                case "move":
-                    System.out.println("Distance: ");
-                    choice1 = scan.nextDouble();
-                    scan.nextLine();
-                    System.out.println("Direction(N/W/E/S): ");
-                    String choice4 = scan.nextLine();
-                    p.move(choice1, choice4);
-                    System.out.println(p);
-                    break;
-                case "totalDistance":
-                    System.out.println(p.totalDistance());
-                    break;
-                case "toString":
-                    System.out.println(p);
-                    break;
-                case "q":
+            String method = scan.nextLine(); //Takes whatever method the individual chooses
+            switch (method) { //Checking what method the user wants and doing stuff based on it.
+                case "move": //If it's the move method,
+                    System.out.println("Distance: "); //User-User 
+                    choice1 = scan.nextDouble(); //Takes How far to go.
+                    scan.nextLine(); //Something so future scanning will cooperate with me.
+                    System.out.println("Direction(N/W/E/S): "); //Something so the program's 
+                    // easier to use by users, I think I'll call it being user-friendly.
+                    String choice4 = scan.nextLine(); //Takes Direction
+                    p.move(choice1, choice4); //Moves the object based on the inputs.
+                    System.out.println(p); //Prints out object so we know about it's position.
+                    break; //Get out of the switch stuff
+                case "totalDistance": //If the method is the totalDistance one
+                    System.out.println(p.totalDistance()); //Shows the total distance
+                    break; //Get out of the switch stuff
+                case "toString": //If the toString method is wanted
+                    System.out.println(p); //Prints out the object
+                    break; //Get out of the switch stuff
+                case "q": //Incase the user wants to run and never look back,
                     System.exit(0); //Ends the program.
-            }
-        }
-    }
-}
+            } //Scanner Bracket
+        } //Forever Loop Encasement Bracket
+    } //Main method bracket
+} //Test Class bracket (This is all the most important of information)
